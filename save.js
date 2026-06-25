b4b6852c3b69d389499eb9a0060783a1409b345d
KasparasBurba <kasparasburba1@gmail.com>
2026-06-25 10:37:13 +0300

    Added offline earnings

--- save.js
+++ save.js
@@ -1,13 +1,16 @@
 function saveGame () {
     const saveData = {
+        lastSaveTime: Date.now(),
         game,
         items: items.map((item) => ({
             id: item.id,
             amount: item.amount,
-            cost: item.cost
+            cost: item.cost,
+            ...(item.multi !== undefined && {multi: item.multi}),
+            ...(item.value !== undefined && {value: item.value})
         }))
     }
-
+    console.log(saveData.lastSaveTime)
     localStorage.setItem("saveString", JSON.stringify(saveData))
 }
 
@@ -30,9 +33,22 @@ function loadGame () {
             if (item) {
                 item.amount = savedItem.amount
                 item.cost = savedItem.cost
+                
+                if (savedItem.multi !== undefined) {
+                    item.multi = savedItem.multi
+                }
+                if (savedItem.value !== undefined) {
+                    item.value = savedItem.value
+                }
             }
         })
 
+        const secondsAway = Math.floor((Date.now() - loadData.lastSaveTime) / 1000)
+        const offlineEarnings = secondsAway * game.coinsPerSecond
+        game.coins += offlineEarnings
+        game.totalCoinsEarned += offlineEarnings
+
+        console.log(offlineEarnings)
         updateAll()
         updateSteal()
     }
